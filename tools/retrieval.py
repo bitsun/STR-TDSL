cb395d79c58c7742d6f00c0caec7727591af0a2d
Bin Liu <b.liu@ivitec.com>
2023-04-06 16:58:36 +0200

    1.suppress grad computation in inference to save gpu memory 2. added retrieval script

--- tools/retrieval.py
+++ tools/retrieval.py
@@ -1,4 +1,3 @@
-
 import argparse
 import sys
 sys.path.append("D:\\code\\Research\\STR-TDSL")
@@ -8,80 +7,20 @@ import torch
 from torch import nn
 from torchvision.transforms import functional as F
 from maskrcnn_benchmark.structures.image_list import ImageList
+from maskrcnn_benchmark.structures.bounding_box import BoxList
 from maskrcnn_benchmark.utils.checkpoint import DetectronCheckpointer
 import cv2
 from  imageio import imread
-from PIL import Image
 import os
 from tqdm import tqdm
+import numpy as np
+from infererence import create_tensor_cv,create_tensor_PIL
 
-def create_tensor_PIL(image_path:str,longer_side:int):
-    """
-    create tensor with PIL image
-    returns image as uint8 numpy array
-    """
-    img = imread(image_path)
-    ori_h, ori_w, _ = img.shape
-    img = Image.fromarray(img.astype(np.uint8)).convert('RGB')
-    #image preprocessing is encoded in the evaluation file
-    if ori_h > ori_w:
-        h = longer_side
-        w = int(ori_w*1.0/ori_h*h)
-    else:
-        w = longer_side
-        h = int(ori_h*1.0/ori_w*w)  
-    pad_h = h if h%32==0 else (h//32+1)*32
-    pad_w = w if w%32==0 else (w//32+1)*32
-    # image resize wiht PIL
-    new_image = Image.Image.resize(img, (pad_w, pad_h), Image.BILINEAR)
-    #new_image = imresize(img.copy(), (pad_h,pad_w))
-    image_tensor = F.to_tensor(new_image)
-    image_tensor = image_tensor[[2, 1, 0]] * 255
-    image_tensor = image_tensor.float()
-    return image_tensor
-
-def create_tensor_cv(image_path:str,longer_side:int):
-    img = imread(image_path)
-    if len(img.shape)==2:
-        #convert gray image to rgb image
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        print(f"image {image_path} is gray image, convert to RGB")
-    if len(img.shape)!=3:
-        print(f"ignoring image {image_path}")
-        return None
-    if img.shape[2]==4:
-        #convert RGBA image to RGB image
-        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
-        print(f"image {image_path} is RGBA image, convert to RGB")
-    elif img.shape[2]==1:
-        #convert gray image to rgb image
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        print(f"image {image_path} is gray image, convert to RGB")
-    elif img.shape[2]!=3:
-        print(f"ignoring image {image_path},which has {img.shape[2]} channels")
-        return None
-    ori_h, ori_w, _= img.shape
-    if ori_h > ori_w:
-        h = longer_side
-        w = int(ori_w*1.0/ori_h*h)
-    else:
-        w = longer_side
-        h = int(ori_h*1.0/ori_w*w)  
-    pad_h = h if h%32==0 else (h//32+1)*32
-    pad_w = w if w%32==0 else (w//32+1)*32
-    new_image = cv2.resize(img, (pad_w, pad_h), interpolation=cv2.INTER_LINEAR)
-    image_tensor = torch.from_numpy(new_image).permute(2,0,1).float()
-    return image_tensor
-
-# "--input","D:\\data\\datasets\\IIIT_STR_V1.0\\imgDatabase",
-#                 "--longer-side","960",
-#                 "--config-file","D:\\code\\Research\\STR-TDSL\\configs\\evaluation.yaml",
-#                 "--ckpt","D:\\data\\datasets\\model_7709.pth"
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="text detection and and its feature extraction")
+    parser = argparse.ArgumentParser(description="retireve text from a given image set")
     parser.add_argument(
         "--input",
-        required=True,
+        required=False,
         help="path to the input image or input image folder"
     )
     parser.add_argument(
@@ -102,7 +41,6 @@ if __name__ == '__main__':
         default=640
     )
     args = parser.parse_args()
-
     model_path = args.ckpt
     cfg.merge_from_file(args.config_file)
     cfg.freeze()
@@ -110,11 +48,10 @@ if __name__ == '__main__':
     checkpointer = DetectronCheckpointer(cfg, model, save_dir=cfg.OUTPUT_DIR)
     _ = checkpointer.load(model_path, use_latest=args.ckpt is None)
     model.eval()
-    #check if torch has cuda
+    model.cuda()
+    
+    
     has_cuda = torch.cuda.is_available()
-
-    if has_cuda:
-        model.cuda()
     #check if os.input_file is dir or file
     if os.path.isdir(args.input):
         #find all images in the folder, with file name extension of .jpg, .png, .jpeg
@@ -122,40 +59,92 @@ if __name__ == '__main__':
     else:
         images = [args.input]
     
+    image_text = []
     for image_path in tqdm(images,"processing images.."):
-    #image_path = "D:\\data\\datasets\\IIIT_STR_V1.0\\imgDatabase\\img_001312.jpg"
-        #read image with pillow
+        if len(image_text)>=1000:
+            break
+    
+        #convert new image to tensor
+        #image_tensor = create_tensor_cv(image_path, int(args.longer_side))
+        #image_tensor = F.normalize(image_tensor, mean=[103.53, 116.28, 123.675], std=[57.375, 57.12, 58.395])
         image_tensor = create_tensor_PIL(image_path, int(args.longer_side))
         image_tensor = F.normalize(image_tensor, mean=[103.53, 116.28, 123.675], std=[57.375, 57.12, 58.395])
         #add batch dimension
         image_tensor = image_tensor.unsqueeze(0)
-        pad_w = image_tensor.shape[3]
-        pad_h = image_tensor.shape[2]
         if has_cuda:
             image_tensor = image_tensor.cuda()
-        
-        imagelist = ImageList(image_tensor, [(pad_h,pad_w)])
+        pad_w = image_tensor.shape[3]
+        pad_h = image_tensor.shape[2]
+        imagelist = ImageList(image_tensor, [(pad_w,pad_h)])
         with torch.no_grad():
             #produce FPN conv features
             features = model.neck(model.backbone(image_tensor))
+            #boxes is detected text boxes, each box is a tensor of shape [N,4]
+            rec_features = features[:len(model.decoder.scales)]
             #features is a list of tensors or feature pyramid, each tensor is a feature map
             #now we can use the internel FCOS detector to detect text boxes or text boxes are provided(from external module) as input
             bboxes, losses = model.decoder.detector(imagelist, features[1:], None)
             bboxes = bboxes[0]
-            #boxes is detected text boxes, each box is a tensor of shape [N,4]
-            rec_features = features[:len(model.decoder.scales)]
             scores = bboxes.get_field("scores")
         
             #box confidence score threshold is 0.2
             pos_idxs = torch.nonzero(scores>0.2).view(-1)#75.43
             confident_bboxes = bboxes[pos_idxs]
+            #bbox provided by external module
+            # x1 = 187/960*pad_w
+            # y1 = 414/540*pad_h
+            # x2 = (187+277)/960*pad_w
+            # y2 = (414+32)/540*pad_h
+            # confident_bboxes = BoxList(torch.tensor([[x1,y1,x2,y2],[x1+20,y1+3,x2+30,y2+2],[x1+40,y1+3,x2+30,y2+5]]), (pad_w,pad_h), mode="xyxy")
             dim = model.decoder.head.image_embedding.rnn.embedding.out_features
             aligned_roi_height = cfg.MODEL.ALIGN.POOLER_RESOLUTION[1]
             if confident_bboxes.bbox.size()[0] == 0:
-                imgs_embedding_nor = torch.zeros([0,aligned_roi_height*dim])
+                #imgs_embedding_nor = torch.zeros([0,aligned_roi_height*dim])
+                continue
             else:
                 rois = model.decoder.head.pooler(rec_features, [confident_bboxes])
                 imgs_embedding = model.decoder.head.image_embedding(rois)
                 #image_embedding is a tensor of shape [N,15,128], N is the number of text boxes
                 imgs_embedding_nor = nn.functional.normalize((imgs_embedding).tanh().view(imgs_embedding.size(0),-1))
-    print(imgs_embedding_nor.shape)
+                #get roi relative coordinates
+                for n in range(confident_bboxes.bbox.size()[0]):
+                    bbox = confident_bboxes.bbox[n]
+                    x1 = bbox[0].item()/pad_w
+                    y1 = bbox[1].item()/pad_h
+                    x2 = bbox[2].item()/pad_w
+                    y2 = bbox[3].item()/pad_h
+                    image_text.append((image_path, (x1,y1,x2,y2),imgs_embedding_nor[n,:].cpu().numpy()))
+    #search for the word "welcome" in the image
+    texts = ['Donaukurief']
+    #torch no_grad
+    query_words_embedding_list = []
+    with torch.no_grad():
+        words = [torch.tensor(model.decoder.head.text_generator.label_map(text.lower())).long().cuda() for text in texts]
+        words_embedding = model.decoder.head.word_embedding(words)
+        #words_embedding_list.append(words_embedding)
+        words_embedding_nor = nn.functional.normalize((words_embedding).tanh().view(words_embedding.size(0),-1))
+        query_words_embedding_list.append(words_embedding_nor[0].cpu().numpy())
+    #search for the word "welcome" in the image
+    similarity_list = []
+    for text_box in image_text:
+        image_path = text_box[0]
+        bbox = text_box[1]
+        imgs_embedding_nor = text_box[2]
+        similarity = np.dot(imgs_embedding_nor, query_words_embedding_list[0])
+        similarity_list.append(similarity)
+        if similarity > 0.6:
+            #print(f"image {image_path} contains the word {texts[0]}")
+            #print(f"bbox: {bbox}")
+            #print(f"similarity score: {similarity}")
+            #load image with cv2
+            image = cv2.imread(image_path)
+            image_width = image.shape[1]
+            image_height = image.shape[0]
+            #draw bbox
+            cv2.rectangle(image, (int(bbox[0]*image_width),int(bbox[1]*image_height)), (int(bbox[2]*image_width),int(bbox[3]*image_height)), (0,255,0), 2)
+            #format a float with two significant digits
+            cv2.putText(image, '{:.3f}'.format(similarity), (int(bbox[0]*image_width),int(bbox[1]*image_height)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            #show image
+            cv2.imshow("image", image)
+    cv2.waitKey(0)
+    print('done')
